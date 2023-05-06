0602249b40fac6a3457a8fabcd8c040f39b8865d
Onur Albayrak <onalbayrakur@gmail.com>
2023-05-06 14:50:26 +0300

    search

--- app/src/main/java/com/albayrakonur/gradapp/SearchFragment.kt
+++ app/src/main/java/com/albayrakonur/gradapp/SearchFragment.kt
@@ -1,10 +1,11 @@
 package com.albayrakonur.gradapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import androidx.fragment.app.Fragment
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,6 +28,25 @@ class SearchFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val searchView = view.findViewById<SearchView>(R.id.searchView)
+        searchView.queryHint = "Type a person name"
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                println(newText)
+                return false
+            }
+        })
     }
 
     override fun onCreateView(
